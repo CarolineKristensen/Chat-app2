cc4fc72d986d1d8433dd3ed4a05459f79538d46e
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-30 15:40:03 +0200

    Call custom Hook from App.js

--- chat-app/src/components/app/App.js
+++ chat-app/src/components/app/App.js
@@ -6,21 +6,12 @@ import {
   } from "react-router-dom";
 import Home from "../home/Home";
 import Login from "../loggedOut/Login";
+import useToken from "./useToken";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../css/Common.css";
 
-function setToken(userToken) {
-  sessionStorage.setItem("token", JSON.stringify(userToken));
-}
-
-function getToken() {
-  const tokenString = sessionStorage.getItem("token");
-  const userToken = JSON.parse(tokenString);
-  return userToken?.token
-}
-
 function App() {
-  const token = getToken();
+  const { token, setToken } = useToken();
 
   if (!token) {
     return <Login setToken={setToken} />
