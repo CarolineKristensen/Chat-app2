6700710d2de0f60b958470a68217445c17d8b22e
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-30 15:12:39 +0200

    Created async function that calls fetch method

--- chat-app/src/components/loggedOut/Login.js
+++ chat-app/src/components/loggedOut/Login.js
@@ -7,7 +7,16 @@ import {
 } from "react-bootstrap";
 import "../css/LoggedOut.css";
 
-
+async function loginUser(credentials) {
+    return fetch("http://localhost:8080/login", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(credentials)
+    })
+    .then(data => data.json())
+}
 
 export default function Login({ setToken }) {
     const [username, setUsername] = useState();
