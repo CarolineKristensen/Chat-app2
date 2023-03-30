223bd078bfd0be7d593fc4b0c7a80255bd268959
feliciabergflo <81237992+feliciabergflo@users.noreply.github.com>
2023-03-30 14:46:25 +0200

    Base

--- chat-app/src/App.js
+++ chat-app/src/App.js
@@ -1,13 +1,30 @@
-import React from "react";
-//import Home from "./components/home/Home";
+import React, { useState } from "react";
+import { 
+  BrowserRouter,
+  Routes,
+  Route 
+  } from "react-router-dom";
+import Home from "./components/home/Home";
 import LoggedOut from "./components/loggedOut/LoggedOut";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./components/css/Common.css";
 
 function App() {
+  const [token, setToken] = useState();
+
+  if (!token) {
+    return <Login setToken={setToken} />
+  }
+
   return (
     <div className="App">
-      <LoggedOut />
+      <h1>Application</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />} />
+          <Route path="/loggedout" element={<LoggedOut />} /> 
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
